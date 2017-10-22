a17049ba2f7800fa9e3fa388ed3367173eec94a0
StrongPosHao <lantian475587063@qq.com>
2017-10-22 18:36:06 +0800

    10/22 6:35PM Commits for backup

---
+++ src/ynu/edu/module/rule/BPMNtoPetri/StartEventRuleTest.java
@@ -0,0 +1,14 @@
+package ynu.edu.module.rule.BPMNtoPetri;
+
+import org.junit.*;
+import org.junit.Test;
+
+public class StartEventRuleTest {
+	
+	@Test
+	public void test() {
+		StartEventRule set = new StartEventRule();
+		
+	}
+	
+}
