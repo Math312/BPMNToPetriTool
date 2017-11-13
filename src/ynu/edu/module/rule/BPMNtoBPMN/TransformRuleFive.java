84d722209651eef16643339dc8b640b6e5fd17ed
seven7817 <1162573719@qq.com>
2017-11-13 21:01:01 +0800

    修改了对规则五出现的所有编排任务都适用的问题

--- src/ynu/edu/module/rule/BPMNtoBPMN/TransformRuleFive.java
+++ src/ynu/edu/module/rule/BPMNtoBPMN/TransformRuleFive.java
@@ -15,19 +15,27 @@ import java.lang.reflect.Method;
 
 public class TransformRuleFive extends AbstractRule{
 	Hashtable<String,LinkedList<String>> allID;
-	String[] choreographyID;//存储信息流的ID
+	String[] choreographyID;//存储编排任务的ID
+	String[] associationID;//存储信息流的ID
 	String [][] IDbyNode ;//得到编排任务周围结点的id
-	String information[];//存储第信息
+	String information[];//存储信息
 
 	@Override
 	public boolean matches(Graphics<BpmnElement> graphics) {
+		int judge = 0;//用来判断是否有两个信息同时连在同一个编排任务上
 		allID = graphics.getIds();
-		if (allID.get(Choreography.class.getName())!=null){
+		if (allID.get(Choreography.class.getName())!=null && allID.get(Association.class.getName()) != null){
 			choreographyID = allID.get(Choreography.class.getName()).toArray(new String[allID.get(Choreography.class.getName()).size()]);
-			for(int i = 0; i < choreographyID.length; i++){
-				GNode g = graphics.getNodeData(choreographyID[i]);
-				Choreography c = (Choreography)g;
-				if (c.getParticipants()[0] != null && c.getParticipants()[1] != null){
+			associationID = allID.get(Association.class.getName()).toArray(new String[allID.get(Association.class.getName()).size()]);
+			for (int i = 0; i < choreographyID.length; i++){
+				for (int j = 0; j < associationID.length; j++){
+					GNode g = graphics.getNodeData(associationID[i]);
+					Association c = (Association)g;
+					if (c.getOutGoing().equals(choreographyID[i])){
+						judge++;
+					}
+				}
+				if (judge == 2){
 					return true;
 				}
 			}
@@ -36,40 +44,59 @@ public class TransformRuleFive extends AbstractRule{
 	}
 	@Override
 	public Graphics<BpmnElement> transfer(Graphics<BpmnElement> graphics) {
-		allID = graphics.getIds();
-		if (allID.get(Choreography.class.getName())!=null){
-			choreographyID = allID.get(Choreography.class.getName()).toArray(new String[allID.get(Choreography.class.getName()).size()]);
-			for (int i = 0; i < choreographyID.length; i++){
-				GNode g = graphics.getNodeData(choreographyID[i]);
-				Choreography c = (Choreography)g;
-				if (c.getParticipants()[0] != null && c.getParticipants()[1] != null){
-					information = c.getParticipants();//存储编排任务的信息之后在移除结点
-					IDbyNode = graphics.getIDbyNode(c.getId());
-					graphics.removeNode(c.getId());
-					
-					
-					Choreography  choreographyOne = new Choreography(Flag.getID(), "choreography", information[0], null);
-					graphics.addNode(choreographyOne);
-					Choreography  choreographyTwo = new Choreography(Flag.getID(), "choreography", null,information[1]);
-					graphics.addNode(choreographyTwo);
-					SequenceFlow sequenceflow = new SequenceFlow(Flag.getID(), choreographyOne.getId(), choreographyTwo.getId());
-					graphics.addNode(sequenceflow);
-					//添加链接
-					graphics.addLink(choreographyOne.getId(), sequenceflow.getId());
-					graphics.addLink(sequenceflow.getId(),choreographyTwo.getId());
-					for(int j = 0 ; j < IDbyNode[1].length; j++)
-					{
-						graphics.addLink(IDbyNode[1][j], choreographyOne.getId() );
-					}
-					for(int j = 0 ; j < IDbyNode[1].length; j++)
-					{
-						graphics.addLink(choreographyTwo.getId(),IDbyNode[0][j]);
-					}
+		int judge = 0;//用来判断是否有两个信息同时连在同一个编排任务上
+		for (int i = 0; i < choreographyID.length; i++){
+			Association c1 = null;
+			Association c2 = null;
+			judge = 0;
+			for (int j = 0; j < associationID.length; j++){
+				GNode g = graphics.getNodeData(associationID[i]);
+				Association c = (Association)g;
+				if (c.getOutGoing().equals(choreographyID[i])){
+					judge++;
+				}
+				if (judge == 1){
+					c1 = c;
 					
 				}
+				if (judge == 2){
+					c2 = c;
+				}
+			}
+			if (judge == 2){
+				GNode g = graphics.getNodeData(choreographyID[i]);
+				Choreography c = (Choreography)g;
+				information = c.getParticipants();
+				IDbyNode = graphics.getIDbyNode(c.getId());
+				graphics.removeNode(c.getId());
+				
+				Choreography  choreographyOne = new Choreography(Flag.getID(), "choreography", information[0], null);
+				graphics.addNode(choreographyOne);
+				Choreography  choreographyTwo = new Choreography(Flag.getID(), "choreography", null,information[1]);
+				graphics.addNode(choreographyTwo);
+				SequenceFlow sequenceflow = new SequenceFlow(Flag.getID(), choreographyOne.getId(), choreographyTwo.getId());
+				graphics.addNode(sequenceflow);
+				
+				
+				//添加链接
+				graphics.addLink(choreographyOne.getId(), sequenceflow.getId());
+				graphics.addLink(sequenceflow.getId(),choreographyTwo.getId());
+				graphics.addLink(c1.getId(), choreographyOne.getId());
+				graphics.addLink(c2.getId(), choreographyTwo.getId());
+				for(int j = 0 ; j < IDbyNode[1].length; j++)
+				{
+					graphics.addLink(IDbyNode[1][j], choreographyOne.getId() );
+				}
+				for(int j = 0 ; j < IDbyNode[1].length; j++)
+				{
+					graphics.addLink(choreographyTwo.getId(),IDbyNode[0][j]);
+				}
+				
+				
 			}
 		}
-		return null;
+
+		return graphics;
 	}
 
 }
